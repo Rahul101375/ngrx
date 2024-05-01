ba85d78e8876d75e95694537e21ca79787194126
Rahulgupta <rahul.gupta@dhwaniris.com>
2024-05-01 19:54:56 +0530

    program-view

--- src/app/container/pages/chart/line/line.component.ts
+++ src/app/container/pages/chart/line/line.component.ts
@@ -9,48 +9,107 @@ Chart.register(...registerables)
   styleUrls: ['./line.component.scss']
 })
 export class LineComponent implements OnInit {
-  @Input() groupBarChartId : string = ""
-  @Input() groupBarData : any = []
+  @Input() lineChartId : string = ""
+  @Input() lineData : any = []
   public chart: Chart | undefined;
   constructor() { }
-
+  ngOnInit(): void {
+    
+  }
+  ngAfterViewInit(){
+    this.line()
+  }
   
-  ngOnInit() {
-    console.log("line",this.groupBarData)
-    this.chart = new Chart("canvas", {
+  line() {
+    this.chart = new Chart(this.lineChartId, {
       type: "bar",
       data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [
-          {
-            label: "# of Votes",
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-              'rgb(0, 133, 90)',
-            ],
-            borderColor: [
-              'rgb(0, 133, 90)',
-            ],
-            borderWidth: 1
-          }
-        ]
+        labels: this.setLabel(this.lineData),
+        datasets: this.setData(this.lineData)
       },
+      options: {
+        indexAxis: 'y',
+        scales: {
+          x: {
+            stacked: true,
+            
+          },
+          y: {
+            stacked: true,
+          }
+        },
+        plugins: {
+          legend: {
+            display: true,
+            position: 'left',
+            onClick: (e:any)=>{
+              e.stopPropagation()
+            },
+            labels: {
+              usePointStyle: true,
+              boxWidth: 6
+            },
+          },
+          datalabels: {
+            color: "white",
+            textAlign:'center',
+          },
+        },
+      }
+      
       // options: {
+      //   aspectRatio: 2.5,
       //   scales: {
-      //     yAxes: [
-      //       {
-      //         ticks: {
-      //           beginAtZero: true
+      //     y: {
+      //       beginAtZero: true,
+      //       grid: {
+      //         color: 'rgba(0, 0, 0, 0.1)',
+      //       }
+      //     },
+      //     x: {
+      //       grid: {
+      //         color: 'rgba(0, 0, 0, 0.1)',
+      //       }
+      //     }
+      //   },
+      //   plugins: {
+      //     tooltip: {
+      //       backgroundColor: 'rgba(0, 0, 0, 0.7)',
+      //       bodyFont: {
+      //         size: 14,
+      //       },
+      //       titleFont: {
+      //         size: 16,
+      //         weight: 'bold',
+      //       }
+      //     },
+      //     legend: {
+      //       labels: {
+      //         font: {
+      //           size: 14,
       //         }
       //       }
-      //     ]
+      //     }
+      //   ,
       //   }
       // }
     });
   }
   setData(data:any){
+    let obj :any ={}
+    console.log("obj-line",data)
+   obj = _.map(data , (el)=>{
+      el.data = [el.value],
+      el.label = "Country",
+      el.backgroundColor = '#00625F'
+      return el;
+    })
+    console.log("data-line-obj",obj);
+    return obj
+  }
+  setLabel(data:any){
     return _.map(data , (el)=>{
-      return
+      return el.key
     })
   }
 }
